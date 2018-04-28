9fb27c05d8efdfd5dc88403c49342be73666acd0
i_jemin <i_jemin@hotmail.com>
2018-04-28 17:21:03 +0900

    Init

---
+++ Assets/VRTeleporter/Sample/SampleVRTeleporterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleVRTeleporterController : MonoBehaviour {
+
+    public VRTeleporter teleporter;
+
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+	}
+}
